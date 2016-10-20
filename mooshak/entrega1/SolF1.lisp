4b96cf2cd43efef8099c6f61ae7e9eac42aeee0f
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2016-10-20 17:30:34 +0100

    Mooshak final version confirmed by rato

--- mooshak/entrega1/SolF1.lisp
+++ mooshak/entrega1/SolF1.lisp
@@ -1,25 +1,22 @@
+; Grupo #xx -- Alice Dourado - 81205 -- Rodrigo Rato - 81500
 
-;;; These functions, and any other ones needed must be implemented
+;;; Includes para a versao a submeter
+(load "datastructures.fas")
+(load "auxfuncs.fas")
 
-;;; Utilizar estes includes para os testes na versao local
-;;; comentar antes de submeter
-(load "datastructures.lisp")
-(load "auxfuncs.lisp")
-
-;;; Utilizar estes includes para a versao a submeter
-; tirar o comentario antes de submeter
-;(load "datastructures.fas")
-;(load "auxfuncs.fas")
-
+; Verifica se uma posicao e' um obstaculo numa pista
 (defun isObstaclep (pos track) 
   (not (nth (second pos) (nth (first pos) (track-env track) ) ) )
 )
 
+; Verifica se um estado e' o estado objectivo
 (defun isGoalp (st) 
   (if (find (state-pos st) (track-endpositions (state-track st)) :test #'equal)
  	 t)
 )
 
+; Devolve a lista em que cada elemento n e dado por l1[n] + l2[n]
+; para listas de comprimentos iguais.
 (defun addListN (l1 l2)
 	(if (= (length l1) (length l2))
 		(if (= 0 (length l1))
@@ -30,8 +27,8 @@
 	)
 )
 
+; Gera o proximo estado a partir do estado st e a accao act
 (defun nextState (st act)
-  	"generate the nextState after state st and action act"
   	(let (newState 
   		 (stVel (addListN (state-vel st) act)))
 
