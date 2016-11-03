b993ea602b734d37dc250200d8d45fc6006bc10d
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2016-11-03 19:03:00 +0000

    Part 2 solution is correct - 14 no mooshak!

--- mooshak/entrega2/SolF2.lisp
+++ mooshak/entrega2/SolF2.lisp
@@ -1,5 +1,9 @@
-(load "datastructures.lisp")
-(load "auxfuncs.lisp")
+;(load "datastructures.lisp")
+;(load "auxfuncs.lisp")
+
+(load "datastructures.fas")
+(load "auxfuncs.fas")
+
 
 
 ;;; TAI position
