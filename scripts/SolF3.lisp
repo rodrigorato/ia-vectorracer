642625a37741d314deebb66a1c3f0bffee27ee65
alice-d <alice.dourado@tecnico.ulisboa.pt>
2016-12-05 17:17:08 +0000

    heuristica

--- scripts/SolF3.lisp
+++ scripts/SolF3.lisp
@@ -73,31 +73,87 @@
 ;;; Pedir 
 (defun nextStates (st)
   "generate all possible next states"
-	(list st))
+  (let ((successors nil))
+    (dolist (act (possible-actions) successors)
+      (let ((new-state (nextState st act)))
+  (if (not (member new-state successors :test #'equalp))
+      (push new-state successors))))))
+
+;;; Solucao e uma seq ordenada de estados
+(defun solution (node)
+  (let ((seq-states nil))
+    (loop 
+      (when (null node)
+  (return))
+      (push (node-state node) seq-states)
+      (setf node (node-parent node)))
+    (values seq-states)))
 
 ;;; limdepthfirstsearch 
-(defun limdepthfirstsearch (problem lim)
-  "limited depth first search
+(defun limdepthfirstsearch (problem lim &key cutoff?)
+  #|limited depth first search
      st - initial state
      problem - problem information
-     lim - depth limit"
-	(list (make-node :state (problem-initial-state problem))) )
-				      
+     lim - depth limit"|#
+  (labels ((limdepthfirstsearch-aux (node problem lim)
+       (if (isGoalp (node-state node))
+     (solution node)
+     (if (zerop lim)
+         :cutoff
+         (let ((cutoff? nil))
+           (dolist (new-state (nextStates (node-state node)))
+       (let* ((new-node (make-node :parent node :state new-state))
+        (res (limdepthfirstsearch-aux new-node problem (1- lim))))
+         (if (eq res :cutoff)
+             (setf cutoff? :cutoff)
+             (if (not (null res))
+           (return-from limdepthfirstsearch-aux res)))))
+           (values cutoff?))))))
+    (let ((res (limdepthfirstsearch-aux (make-node :parent nil :state (problem-initial-state problem))
+          problem
+          lim)))
+      (if (eq res :cutoff)
+    (if cutoff?
+        :cutoff
+        nil)
+    res))))				      
 
 ;iterlimdepthfirstsearch
-(defun iterlimdepthfirstsearch (problem)
-  "limited depth first search
+(defun iterlimdepthfirstsearch (problem &key (lim most-positive-fixnum))
+  #|limited depth first search
      st - initial state
      problem - problem information
-     lim - limit of depth iterations"
-	(list (make-node :state (problem-initial-state problem))) )
+     lim - limit of depth iterations"|#
+  (let ((i 0))
+    (loop
+      (let ((res (limdepthfirstsearch problem i :cutoff? T)))
+  (when (and res (not (eq res :cutoff)))
+    (return res))
+  (incf i)
+  (if (> i lim)
+      (return nil))))))
 	
 ;; Solution of phase 3
 
 ;; Heuristic
+(defun subPair (pair1 pair2)
+  (list (- (first pair1) (first pair2)) (- (second pair1) (second pair2)))
+)
+
 (defun compute-heuristic (st)
-	0)
-	  
+  (let ( (track1 (state-track st)) (temp nil) (minDistance '(999 999)))
+    (loop for end in (track-endpositions track1) do
+      (setf temp (subPair end (state-pos st)))
+      (if (< (first temp) (first minDistance))
+          (setf (first minDistance) (first temp))
+      )
+      (if (< (second temp) (second minDistance))
+        (setf (second minDistance) (second temp))
+      )
+    )
+    (max (first minDistance) (second minDistance))
+  )
+)
   
 	    
 ;;; A*
